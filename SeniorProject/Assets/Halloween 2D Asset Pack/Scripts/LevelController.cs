9428d6f9ea8bdc098ceeee992af629a5d2aa2e6b
an6688 <anil.alston@oit.edu>
2020-09-23 22:48:43 -0700

    revised sprites
    imported animations from Spine, new assets from Unity store.

---
+++ SeniorProject/Assets/Halloween 2D Asset Pack/Scripts/LevelController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelController : MonoBehaviour {
+
+
+	public void LoadScene (string sceneN) {
+		SceneManager.LoadScene (sceneN);
+	}
+	//Load empty scene
+	
+	  public void ASceneLoad(string sceneN)
+    {
+        SceneManager.LoadScene(sceneN,LoadSceneMode.Single);
+    }
+
+    public void AsyncLoad ()
+    {
+        StartCoroutine(LoadYourAsyncScene());
+    }
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Game_Scene");
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
---
+++ SeniorProject/Assets/Halloween 2D Asset Pack/Scripts/NavManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class NavManager : MonoBehaviour
+{
+    private int index;
+    private GameObject currentPanel;
+
+    [SerializeField]
+    private TMP_Text titleTxt;
+    [SerializeField]
+    private string[] panelTitles;
+    [SerializeField]
+    private GameObject[] panels;
+    [SerializeField]
+    private Animator hellhoundAnim;
+
+    void Start()
+    {
+        SetActivePanel(0);
+    }
+    public void NextPanel()
+    {
+        SetActivePanel((int)Mathf.Repeat(++index, panels.Length));
+    }
+    public void PrevPanel()
+    {
+        SetActivePanel((int)Mathf.Repeat(--index, panels.Length));
+    }
+    private void SetActivePanel(int index)
+    {
+        if (currentPanel != null)
+            currentPanel.SetActive(false);
+
+        titleTxt.text = panelTitles[index];
+        currentPanel = panels[index];
+
+        currentPanel.SetActive(true);
+    }
+
+    public void SwitchAnimation()
+    {
+        switch (hellhoundAnim.GetBool("Run"))
+        {
+            case true: hellhoundAnim.SetBool("Run",false);
+                break;
+            case false: hellhoundAnim.SetBool("Run", true);
+                break;
+        }
+    }
+
+}
+
